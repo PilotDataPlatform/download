b884af1f96e9ff3b0ae47863d86899ab39b6682c
zhiren <zzhan@indocresearch.org>
2022-05-10 12:22:00 -0400

    update the test case of project file

--- tests/routers/v2/test_project_file_pre_download.py
+++ tests/routers/v2/test_project_file_pre_download.py
@@ -18,18 +18,10 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 
-async def test_v2_download_pre_return_400_when_project_code_and_dataset_geid_are_missing(client):
+async def test_v2_download_pre_return_422_when_container_code_and_type_are_missing(client):
     resp = await client.post('/v2/download/pre/', json={'session_id': '123', 'operator': 'me', 'files': [{}]})
 
-    assert resp.status_code == 400
-    assert resp.json() == {
-        'code': 400,
-        'error_msg': 'project_code or dataset_geid required',
-        'page': 0,
-        'total': 1,
-        'num_of_pages': 1,
-        'result': [],
-    }
+    assert resp.status_code == 422
 
 
 async def test_v2_download_pre_return_500_when_fail_to_add_files_to_zip(
@@ -49,15 +41,15 @@ async def test_v2_download_pre_return_500_when_fail_to_add_files_to_zip(
             'session_id': '123',
             'operator': 'me',
             'project_id': 'any',
-            'dataset_geid': 'fake_geid',
-            'project_code': 'any_project_code',
+            'container_code': 'fake_geid',
+            'container_type': 'project',
             'files': [{'geid': 'fake_geid'}],
         },
     )
     assert resp.status_code == 500
     assert resp.json() == {
         'code': 500,
-        'error_msg': '[Internal] api_data_download File fake_geid does not exist',
+        'error_msg': '[Internal] api_data_download resource fake_geid does not exist',
         'page': 0,
         'total': 1,
         'num_of_pages': 1,
@@ -65,7 +57,7 @@ async def test_v2_download_pre_return_500_when_fail_to_add_files_to_zip(
     }
 
 
-async def test_v2_download_pre_return_200_when_approval_request_id(client, httpx_mock, metadata):
+async def test_v2_download_pre_return_200_when_approval_request_id(client, httpx_mock, metadata, mock_minio):
     httpx_mock.add_response(
         method='GET',
         url='http://metadata_service/v1/item/fake_geid/',
@@ -90,32 +82,27 @@ async def test_v2_download_pre_return_200_when_approval_request_id(client, httpx
     resp = await client.post(
         '/v2/download/pre/',
         json={
-            'session_id': '123',
             'operator': 'me',
             'project_id': 'any',
-            'project_code': 'fake_code',
+            'container_code': 'fake_code',
+            'container_type': 'project',
             'files': [{'geid': 'fake_geid'}],
             'approval_request_id': '67e6bf62-be82-4401-9ec0-7d49ee047fe7',
         },
     )
-    result = resp.json()['result']
+
     assert resp.status_code == 200
-    assert result['session_id'] == '123'
+    result = resp.json()['result']
     assert result['job_id']
-    assert result['geid'] == 'fake_geid'
     assert 'obj/path' in result['source']
     assert result['action'] == 'data_download'
     assert result['status'] == 'ZIPPING'
     assert result['project_code'] == 'fake_code'
     assert result['operator'] == 'me'
-    assert result['progress'] == 0
     assert result['payload']['hash_code']
 
 
-async def test_v2_download_pre_return_200_when_label_is_not_Folder(
-    client,
-    httpx_mock,
-):
+async def test_v2_download_pre_return_200_when_label_is_not_Folder(client, httpx_mock, metadata, mock_minio):
     httpx_mock.add_response(
         method='GET',
         url='http://metadata_service/v1/item/fake_geid/',
@@ -143,28 +130,24 @@ async def test_v2_download_pre_return_200_when_label_is_not_Folder(
             'session_id': '123',
             'operator': 'me',
             'project_id': 'any',
-            'project_code': 'any_project_code',
+            'container_code': 'any_project_code',
+            'container_type': 'project',
             'files': [{'geid': 'fake_geid'}],
         },
     )
-    result = resp.json()['result']
+
     assert resp.status_code == 200
-    assert result['session_id'] == '123'
+    result = resp.json()['result']
     assert result['job_id']
-    assert result['geid'] == 'fake_geid'
     assert 'obj/path' in result['source']
     assert result['action'] == 'data_download'
     assert result['status'] == 'ZIPPING'
     assert result['project_code'] == 'any_project_code'
     assert result['operator'] == 'me'
-    assert result['progress'] == 0
     assert result['payload']['hash_code']
 
 
-async def test_v2_download_pre_return_200_when_label_is_Folder(
-    client,
-    httpx_mock,
-):
+async def test_v2_download_pre_return_200_when_label_is_Folder(client, httpx_mock, metadata, mock_minio):
     httpx_mock.add_response(
         method='GET',
         url='http://metadata_service/v1/item/fake_geid/',
@@ -175,7 +158,7 @@ async def test_v2_download_pre_return_200_when_label_is_Folder(
                 'storage': {'location_uri': 'http://anything.com/bucket/obj/path'},
                 'id': 'fake_geid',
                 'type': 'folder',
-                'operator': 'me',
+                'owner': 'me',
                 'parent_path': 'admin',
                 'container_code': 'fake_project_code',
                 'zone': 0,
@@ -186,17 +169,17 @@ async def test_v2_download_pre_return_200_when_label_is_Folder(
 
     httpx_mock.add_response(
         method='GET',
-        url='http://metadata_service/v1/items/search/?container_code=fake_project_code&zone=0&'
-        'recursive=true&archived=false&parent_path=admin.fake_file&owner=me',
+        url='http://metadata_service/v1/items/search/?container_code=fake_project_code'
+        '&container_type=project&zone=0&recursive=true&archived=false&parent_path=admin.fake_file&owner=me',
         json={
-            'results': [
+            'result': [
                 {
                     'code': 'any_code',
                     'labels': 'any_label',
                     'storage': {'location_uri': 'http://anything.com/bucket/obj/path'},
                     'id': 'fake_geid',
                     'type': 'file',
-                    'operator': 'me',
+                    'owner': 'me',
                     'parent_path': 'admin',
                     'container_code': 'fake_project_code',
                     'zone': 0,
@@ -214,19 +197,18 @@ async def test_v2_download_pre_return_200_when_label_is_Folder(
             'session_id': '123',
             'operator': 'me',
             'project_id': 'any',
-            'project_code': 'any_project_code',
+            'container_code': 'fake_project_code',
+            'container_type': 'project',
             'files': [{'geid': 'fake_geid'}],
         },
     )
-    result = resp.json()['result']
+
     assert resp.status_code == 200
-    assert result['session_id'] == '123'
+    result = resp.json()['result']
     assert result['job_id']
-    assert result['geid'] == 'fake_geid'
     assert result['project_code'] in result['source']
     assert result['action'] == 'data_download'
     assert result['status'] == 'ZIPPING'
-    assert result['project_code'] == 'any_code'
+    assert result['project_code'] == 'fake_project_code'
     assert result['operator'] == 'me'
-    assert result['progress'] == 0
     assert result['payload']['hash_code']
